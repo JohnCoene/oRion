9d3167e23f1b3daa315f443327ea99d1ae063c65
jcoenep <jcoenep@gmail.com>
2016-03-07 19:53:48 +0800

    reportInput added

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,6 @@
 
 # construct credentials
-construct.cred <- function(json, pars) {
+constructCred <- function(json, pars) {
   
   credentials <- list(client.id = pars$client.id,
                       client.secret = pars$client.secret,
@@ -16,23 +16,19 @@ construct.cred <- function(json, pars) {
   
 }
 
-parse.cred <- function(response){
+testReturn <- function(response) {
   
   json <- jsonlite::fromJSON(rawToChar(response$content))
   
   # check if success
   if(json$status != 200){
     
-    msg <- paste0("OAuth error. API returned status ", json$status, 
+    msg <- paste0("API returned status ", json$status, 
                   ": ", json$message)
     
     stop(msg, call. = FALSE)
     
-  } else {
-    
-    return(json)
-    
-  }
+  } 
   
 }
 
@@ -40,7 +36,7 @@ orionToken <- function() {
   
   cred <- tryCatch(get("credentials", envir=cred_env),
                    error = function(e){
-                     stop("No credentials see orionOAuth", call. = FALSE)
+                     stop("No credentials see ?orionOAuth", call. = FALSE)
                    })
   
   if(cred$expires_in_date <= Sys.time()) {
@@ -55,10 +51,10 @@ orionToken <- function() {
 
 checkObjects <- function(objects){
   
-  valid <- c("campaign", "ad", "adset", "audience")
+  valid <- c("campaign", "ad", "adset", "audience", "image")
   
   sapply(objects, function(x) {
-    if(objects %in% valid == FALSE){
+    if(x %in% valid == FALSE){
       stop("invalid object", call. = FALSE)
     }
   })
@@ -77,7 +73,44 @@ encodeBody <- function(body){
 }
 
 parseJSON <- function(data){
+  
   data <- as.data.frame(t(unlist(data)), stringsAsFactors = FALSE)
   
   return(data)
+}
+
+
+reportInput <- function(body){
+  
+  col <- c("impression", "click", "ctr", "revenue", "cpm", "conversion", 
+           "videoview")
+  groups = c("datetime", "age", "gender", "location","brand", "ad", "adset", 
+             "campaign", "videotype")
+  
+  sapply(body$column, function(x){
+    if(!x %in% col) {
+      stop(paste0("invalid column. valid values are ", 
+                  paste0(col, collapse = ", ")), call. = FALSE)
+    }
+  })
+  
+  sapply(body$group.by, function(x){
+    if(!x %in% groups) {
+      stop(paste0("invalid column. valid values are ", 
+                  paste0(groups, collapse = ", ")), call. = FALSE)
+    }
+  })
+  
+  if(length(body$groupby) == 1) {
+    
+    stop("must specify at least 2 arguments for group.by", call. = FALSE)
+    
+  }
+  
+  if(length(body$column) == 1) {
+    
+    stop("must specify at least 2 arguments for column", call. = FALSE)
+    
+  }
+  
 }
