8af598cd3462000bfa31e1aa05683bb0b9fa3254
jcoenep <jcoenep@gmail.com>
2016-03-21 17:21:01 +0800

    add tests to map FUN

--- tests/testthat/test_map.R
+++ tests/testthat/test_map.R
@@ -12,4 +12,11 @@ test_that("init", {
   
   expect_equal(names(map), c("source.id", "target.id", "source.name", 
                              "target.name", "source.object", "target.object"))
+  
+  expect_error(map(campaigns, adsets, ads))
+  expect_error(map(campaigns, adsets, audiences))
+  expect_error(map(campaigns, ads, audiences))
+  expect_error(map(adsets, ads, audiences))
+  
+  expect_error(map(adsets, campaigns, ads, audiences))
 })
