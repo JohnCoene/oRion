2ecd3c16ecace010c485942cf4cca4f33804b73e
jcoenep <jcoenep@gmail.com>
2016-03-28 15:46:12 +0800

    mising data.frames

--- tests/testthat/test_map.R
+++ tests/testthat/test_map.R
@@ -12,9 +12,9 @@ test_that("init", {
   map <- map(campaigns, adsets, ads, audiences)
   expect_equal(names(map), c("source.id", "target.id", "source.name",
                              "target.name", "source.object", "target.object"))
-  expect_error(map(campaigns, adsets, ads))
-  expect_error(map(campaigns, adsets, audiences))
-  expect_error(map(campaigns, ads, audiences))
-  expect_error(map(adsets, ads, audiences))
-  expect_error(map(adsets, campaigns, ads, audiences))
+  expect_error(map(adsets = adsets, ads = ads, audiences = audiences))
+  expect_error(map(campaigns = campaigns, ads = ads, audiences = audiences))
+  expect_error(map(campaigns = campaigns, adsets = adsets, 
+                   audiences = audiences))
+  expect_error(map(campaigns = campaigns, adsets = adsets, ads = ads))
 })
